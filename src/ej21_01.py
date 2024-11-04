861ad5322c57f8f132653755fe18c04b86d0de4a
dhindalecio <dhindalecio@iesrafaelalberti.es>
2024-11-04 15:19:44 +0100

    Cambios

--- src/ej21_01.py
+++ src/ej21_01.py
@@ -1,13 +1,16 @@
 # ejercicio 21_01
 
+def verificar_edad(edad):
+    if edad >= 18:
+        return "Eres mayor de 18 años"
+    else:
+        return"No eres mayor de 18 años enano"
+
 def main():
 
     edad = int(input("Indica tu edad por favor: "))
+    print(verificar_edad(edad))
 
-    if edad >= 18:
-        print("Eres mayor de 18 años")
-    else:
-       print("No eres mayor de 18 años enano")
-
+    
 if __name__ == "__main__":
     main()
